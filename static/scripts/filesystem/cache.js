6c049e9daea8c54874d83b18e3d2dee684cabc56
Dennis Li <dennis.li.92@gmail.com>
2014-04-12 14:06:34 -0400

    Room File Lister completed. Detects dupes with file.name. filerequested event sent when user clicks link

--- static/scripts/filesystem/cache.js
+++ static/scripts/filesystem/cache.js
@@ -1,63 +1,63 @@
 var cache = (function(app, LocalLister, FileSystem, ObservableArray) {
-	'use strict';
+  'use strict';
 
-	var fs = new FileSystem(1024*1024*1024),
-		room_directory = fs.getRoom(app.room);
+  var fs = new FileSystem(1024*1024*1024),
+    room_directory = fs.getRoom(app.room);
 
-	var cache = new ObservableArray();
-	// handle adds on the cache
-	cache.listen(LocalLister.onfilesloaded.bind(LocalLister));
+  var cache = new ObservableArray();
+  // handle adds on the cache
+  cache.listen(LocalLister.onfilesloaded.bind(LocalLister));
 
-	// handle removes
-	cache.listen(function(cleared, type) {
-		if (type === 'remove') {
-			room_directory.then(function(room) {
-				return room.removeRecursively();
-			}).then(function() {
-				room_directory = fs.getRoom(app.room);
-			});
-		}
-	});
+  // handle removes
+  cache.listen(function(cleared, type) {
+    if (type === 'remove') {
+      room_directory.then(function(room) {
+        return room.removeRecursively();
+      }).then(function() {
+        room_directory = fs.getRoom(app.room);
+      });
+    }
+  });
 
-	// handle files that are already cached in the room
-	fs.getRoomFiles(app.room).then(function(files) {
-		cache.extend(files);
-	});
+  // handle files that are already cached in the room
+  fs.getRoomFiles(app.room).then(function(files) {
+    cache.extend(files);
+  });
 
-	// handle arriving imports
-	window.addEventListener('imported', function(event) {
-		var files = event.detail;
-		
-		room_directory.then(function(room) {
-			var promises = files.map(function(file, i) {
-				return room.makeFileEntry(file.name, true).catch(function() {
-					return false;
-				});
-			});
+  // handle arriving imports
+  window.addEventListener('imported', function(event) {
+    var files = event.detail;
 
-			return Promise.all(promises);
-		}).then(function(entries) {
-			var duplicates = [];
-			entries = entries.filter(function(file, i) { 
-				if (!file) {
-					duplicates.push(files[i]);
-				}
-				return file; 
-			});
+    room_directory.then(function(room) {
+      var promises = files.map(function(file, i) {
+        return room.makeFileEntry(file.name, true).catch(function() {
+          return false;
+        });
+      });
 
-			duplicates.forEach(function(duplicate) {
-				files.splice(files.indexOf(duplicate), 1);
-			});
+      return Promise.all(promises);
+    }).then(function(entries) {
+      var duplicates = [];
+      entries = entries.filter(function(file, i) {
+        if (!file) {
+          duplicates.push(files[i]);
+        }
+        return file;
+      });
 
-			var promises = entries.map(function(entry, i) {
-				entry.write(files[i]);
-			});
+      duplicates.forEach(function(duplicate) {
+        files.splice(files.indexOf(duplicate), 1);
+      });
 
-			return Promise.all(promises);
-		}).then(function() {
-			cache.extend(files);
-		}).catch(app.logError);
-	});
+      var promises = entries.map(function(entry, i) {
+        entry.write(files[i]);
+      });
 
-	return cache;
+      return Promise.all(promises);
+    }).then(function() {
+      cache.extend(files);
+    }).catch(app.logError);
+  });
+
+  return cache;
 })(window.app, window.LocalLister, window.FileSystem, window.ObservableArray)
